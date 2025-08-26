d931a5ffc296ecba5f363e565f999cadfe3dbf8b
HyeongJun, Kim <khj029956@naver.com>
2025-08-26 16:01:32 +0900

    FEat: 이미지 주소 링크 변경

--- netflix-clone-codding/src/data/mockHome.ts
+++ netflix-clone-codding/src/data/mockHome.ts
@@ -9,7 +9,7 @@ export const HERO: MediaItem = {
   id: 'runningman',
   title: '런닝맨',
   // 아무 웹 이미지 링크 사용 가능 (예시는 TMDB 표준 사이즈 경로)
-  backdrop: 'https://image.tmdb.org/t/p/w1280/ogbVw0qj5vZx5lq7m1J1mHqKfpr.jpg',
+  backdrop: 'https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2021%2F12%2FNetflix-dont-look-up-new-character-poster-image-00.jpg?q=75&w=800&cbr=1&fit=max',
   poster:   'https://image.tmdb.org/t/p/w342/6oC2K9P6u1U0f0h5VnqQOe1Nqv2.jpg'
 };
 
@@ -27,8 +27,8 @@ export const ROW_TOP10: MediaItem[] = [
 ];
 
 export const ROW_CONTINUE: MediaItem[] = [
-  { id:'a', title:'에스콰이어', poster:'https://image.tmdb.org/t/p/w342/a.jpg', backdrop:'' },
-  { id:'b', title:'기생충',   poster:'https://image.tmdb.org/t/p/w342/b.jpg', backdrop:'' },
-  { id:'c', title:'나는 생존자다', poster:'https://image.tmdb.org/t/p/w342/c.jpg', backdrop:'' },
-  { id:'d', title:'솔로지옥', poster:'https://image.tmdb.org/t/p/w342/d.jpg', backdrop:'' },
+  { id:'a', title:'웬즈데이', poster:'https://occ-0-8133-58.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABUAC9EWlFiqOqh7Q6KuMgzpugwCBsBZvDnfG-vm4EE8-yKUDgRnEEY4VkgHF39pwtib77ekvwQx1izTzYMAFNzSU4LMHUu05nKpOV2KrFhbsiM56MmVPFvzNkSo9Z55k5UJ64awcg4ZnoCdl_EEecpwVD99dhiz4tD0mY_0ewhuUw4vtGh09JNPDapZWT0BBtcYzaPQWp3U.jpg?r=bfa', backdrop:'' },
+  { id:'b', title:'기생충',   poster:'https://occ-0-8133-58.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABbEMLH3v_rh8xnTLuxcSGnMaGe30Oo07oJNnGgso8rVC34hCP5-SSv8y838_aR5BMTYoFZdsleetf1XHYqlXeSgPfHlsmiz4qIU.webp?r=720', backdrop:'' },
+  { id:'c', title:'미스터션샤인', poster:'https://occ-0-8133-58.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABZgbsQOs2PnybPcB1tyaEpYfzBi7WKzuckCA3zBdxN7ii5z3llt-rbAIv8QYYcVfnNzJr8-K3p8KEEhUscCYpdo9F5UWRarc-qE.webp?r=050', backdrop:'' },
+  { id:'d', title:'솔로지옥', poster:'https://occ-0-8133-58.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABeXTGITfBq5ScPdPyFKDcOir8yIWsPp2KKBK_K83ioVCdU972YF0_3AUvT8jz8SAEzyCq5G21QjY7R-8-nbLhtvecWD91YKl5cA.webp?r=f62', backdrop:'' },
 ];
